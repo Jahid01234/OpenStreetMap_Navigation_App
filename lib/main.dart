45b263f80c92544c7c0e2be82c33cab9c75a492d
Jahid01234 <mdjahidhasan790523@gmail.com>
2026-07-12 20:03:18 +0600

    - ......

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,22 @@ import 'package:flutter/services.dart';
 import 'package:open_streetmap_app/app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Force portrait orientation
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-
-  // Dark status bar for the map
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: Color(0xFF1A2332),
-    ),
-  );
+  // WidgetsFlutterBinding.ensureInitialized();
+  //
+  // // Force portrait orientation
+  // await SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
+  //
+  // // Dark status bar for the map
+  // SystemChrome.setSystemUIOverlayStyle(
+  //   const SystemUiOverlayStyle(
+  //     statusBarColor: Colors.transparent,
+  //     statusBarIconBrightness: Brightness.light,
+  //     systemNavigationBarColor: Color(0xFF1A2332),
+  //   ),
+  // );
   runApp(const MyApp());
 }
 
